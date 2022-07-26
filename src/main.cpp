936a45921d99dbf324deb09e77d2fb4e7cf539e2
Robotics & Big Data Labs University of Haifa <rbdlabhaifa@gmail.com>
2022-07-26 19:18:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * that best brings the 3D points to their corresponding 2D points.
  */
 
-#include "RansacOptimalPnP/RansacOptimalPnP.h"
+#include "NewtonPnP/NewtonPnP.h"
 #include "FramePose/FramePose.h"
 #include <opencv2/imgproc.hpp>
 #include "opencv2/core/core.hpp"
@@ -13,67 +13,67 @@
 #include <string>
 #include <vector>
 
+
 std::vector<cv::Point2d> Generate2DPoints();
 std::vector<cv::Point3d> Generate3DPoints();
 
 std::vector<cv::Point2d> Generate2DPoints(){
-  std::vector<cv::Point2d> points;
- 
-  float x,y;
- 
-  x=282;y=274;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=397;y=227;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=577;y=271;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=462;y=318;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=270;y=479;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=450;y=523;
-  points.push_back(cv::Point2d(x,y));
- 
-  x=566;y=475;
-  points.push_back(cv::Point2d(x,y));
- 
-  return points;
+    std::vector<cv::Point2d> points;
+
+    double x,y;
+
+    x=282;y=274;
+    points.push_back(cv::Point2d(x,y));
+
+    x=397;y=227;
+    points.push_back(cv::Point2d(x,y));
+
+    x=577;y=271;
+    points.push_back(cv::Point2d(x,y));
+
+    x=462;y=318;
+    points.push_back(cv::Point2d(x,y));
+
+    x=270;y=479;
+    points.push_back(cv::Point2d(x,y));
+
+    x=450;y=523;
+    points.push_back(cv::Point2d(x,y));
+
+    x=566;y=475;
+    points.push_back(cv::Point2d(x,y));
+
+    return points;
 }
  
  
 std::vector<cv::Point3d> Generate3DPoints(){
-  std::vector<cv::Point3d> points;
- 
- 
-  float x,y,z;
- 
-  x=.5;y=.5;z=-.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=.5;y=.5;z=.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=-.5;y=.5;z=.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=-.5;y=.5;z=-.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=.5;y=-.5;z=-.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=-.5;y=-.5;z=-.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  x=-.5;y=-.5;z=.5;
-  points.push_back(cv::Point3d(x,y,z));
- 
-  return points;
+    std::vector<cv::Point3d> points;
+
+    double x,y,z;
+
+    x=.5;y=.5;z=-.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=.5;y=.5;z=.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=-.5;y=.5;z=.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=-.5;y=.5;z=-.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=.5;y=-.5;z=-.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=-.5;y=-.5;z=-.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    x=-.5;y=-.5;z=.5;
+    points.push_back(cv::Point3d(x,y,z));
+
+    return points;
 }
 
 
@@ -81,9 +81,6 @@ int main(){
 
     std::vector<cv::Point2d> imagePoints = Generate2DPoints();
     std::vector<cv::Point3d> objectPoints = Generate3DPoints();
- 
-    cv::Mat rvec(3,1,cv::DataType<double>::type);
-    cv::Mat tvec(3,1,cv::DataType<double>::type);
 
     cv::Mat distCoeffs(4,1,cv::DataType<double>::type);
     distCoeffs.at<double>(0) = 0;
@@ -92,23 +89,19 @@ int main(){
     distCoeffs.at<double>(3) = 0;
     distCoeffs.at<double>(4) = 0;
 
-    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 647.81841563, 0, 335.8814632, 0, 645.9438274, 225.99776891, 0, 0, 1);
-    int min_iterations = 10, max_iterations = 10, min_inliers = 8;
-    double reprojection_err_th = 64.0;
-
-    RansacOptimalPnP RansacPnPSolver(min_iterations, max_iterations, min_inliers, reprojection_err_th, cameraMatrix, distCoeffs);
-
+    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 640, 0, 480, 0, 640, 480, 0, 0, 1);
     FramePose frame_pose;
-    std::vector<cv::Point2d> undist_scene;
-    cv::undistortPoints(imagePoints, undist_scene, cameraMatrix, distCoeffs, cv::Mat(), cameraMatrix);
 
-    bool success_optimal = RansacPnPSolver.newton_pnp(undist_scene, objectPoints, frame_pose);
-    
-    for (int i=0; i < 3; i++){
-        std::cout  << frame_pose.R.at<float>(i,0) << "," << frame_pose.R.at<float>(i,1) << "," << frame_pose.R.at<float>(i,2) << std::endl;
-    }
+    // -----------------------------------------
+    // Newton PnP:
+    // -----------------------------------------
+    NewtonPnP NewtonPnP(cameraMatrix, distCoeffs);
+    bool success_optimal = NewtonPnP.newton_pnp(imagePoints, objectPoints, frame_pose);
 
-    std::cout << frame_pose.t << std::endl;
+    std::cout << "R: " << std::endl << frame_pose.R << std::endl << std::endl;
+    std::cout << "t: " << std::endl << frame_pose.t << std::endl;
+
+    // -----------------------------------------
 
     return 0;
 }
